0ffb8dc8d20ec923d2762f9a3f7b5cb6d0e605de
dmitry <dima.lenchevskii@gmail.com>
2020-05-10 17:05:51 +0300

    solve Collatz

--- collatz-conjecture/src/CollatzConjecture.hs
+++ collatz-conjecture/src/CollatzConjecture.hs
@@ -1,8 +1,19 @@
+-- module CollatzConjecture (collatz) where
+-- import Data.List(genericLength)
+
+-- collatz :: Integer -> Maybe Integer
+-- collatz n   | n > 0     = Just (genericLength (collatzSeq n) - 1)
+--             | otherwise = Nothing
+--                 where   collatzSeq 0    = [0]
+--                         collatzSeq 1    = [1]
+--                         collatzSeq x    | even x    = x : collatzSeq (div x 2)
+--                                         | otherwise = x : collatzSeq (3 * x + 1)
+
 module CollatzConjecture (collatz) where
 
 collatz :: Integer -> Maybe Integer
-collatz number  | number == 1   = Just 1
-                | even number   = collatz (div number 2)
-                | otherwise     = collatz (3 * number + 1)
-
--- foldr 
+collatz n
+    | n <= 0 = Nothing
+    | n == 1 = Just 0
+    | mod n 2 == 0 = fmap (+1) $ collatz (n `div` 2)
+    | otherwise = fmap (+1) $ collatz (n*3+1)
