675d2584436328c2f3ef0a13dd1a77d8e9e8c83e
dmitry <dima.lenchevskii@gmail.com>
2020-06-30 11:30:38 +0300

    refactoring

--- rna-transcription/src/DNA.hs
+++ rna-transcription/src/DNA.hs
@@ -1,14 +1,9 @@
 module DNA (toRNA) where
 
 toRNA :: String -> Either Char String
-toRNA xs    | null xs       = Right ""
-            | xs == "G"     = Right "C"
-            | xs == "C"     = Right "G"
-            | xs == "T"     = Right "A"
-            | xs == "A"     = Right "U"
-            | otherwise     = traverse charToRNA xs
-                where charToRNA x   | x == 'G'  = Right 'C'
-                                    | x == 'C'  = Right 'G'
-                                    | x == 'T'  = Right 'A'
-                                    | x == 'A'  = Right 'U'
-                                    | otherwise = Left x
+toRNA   = traverse charToRNA
+            where charToRNA x   | x == 'G'  = Right 'C'
+                                | x == 'C'  = Right 'G'
+                                | x == 'T'  = Right 'A'
+                                | x == 'A'  = Right 'U'
+                                | otherwise = Left x
