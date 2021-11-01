a6b32180093f0a04adafb5b328328c17d7cef3d3
Dzmitry <dima.lenchevskii@gmail.com>
2021-11-01 14:19:30 +0300

    beautified Collatz best solution

--- collatz-conjecture/src/CollatzConjectureBestSolution.hs
+++ collatz-conjecture/src/CollatzConjectureBestSolution.hs
@@ -2,10 +2,10 @@ module CollatzConjecture (collatz) where
 
 collatz :: Integer -> Maybe Integer
 collatz n
-    | n <= 0 = Nothing
-    | n == 1 = Just 0
-    | mod n 2 == 0 = fmap (+1) $ collatz (n `div` 2)
-    | otherwise = fmap (+1) $ collatz (n * 3 + 1)
+  | n <= 0 = Nothing
+  | n == 1 = Just 0
+  | mod n 2 == 0 = fmap (+ 1) $ collatz (n `div` 2)
+  | otherwise = fmap (+ 1) $ collatz (n * 3 + 1)
 
 -- (Beautified)
 collatzB :: Integer -> Maybe Integer
